0e4a3915633bcbef210a0d3fe59ae81d8633a834
Wenxin Wang <wenxin@pony.ai>
2025-02-08 11:52:38 +0800

    destop-environments: fix environ loading for Ubuntu 18.04 Gnome

--- bootstrap/desktop-environments/profile.d/71-ssh_askpass.sh
+++ bootstrap/desktop-environments/profile.d/71-ssh_askpass.sh
@@ -1,4 +1,6 @@
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-# export SSH_ASKPASS=ssh-askpass
-export SSH_ASKPASS=/usr/bin/ksshaskpass
-export SSH_ASKPASS_REQUIRE=prefer
+
+if [[ "$XDG_CURRENT_DESKTOP" =~ .*KDE ]]; then
+    export SSH_ASKPASS=/usr/bin/ksshaskpass
+    export SSH_ASKPASS_REQUIRE=prefer
+fi
