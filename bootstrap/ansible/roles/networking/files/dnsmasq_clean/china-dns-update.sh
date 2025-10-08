c5cc94e1c9698cd41f8c5d5228189a97bf2f9118
Wenxin Wang <i@wenxinwang.me>
2025-10-08 22:14:22 +0800

    networking: dnsmasq clean

---
+++ bootstrap/ansible/roles/networking/files/dnsmasq_clean/china-dns-update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+WORKDIR=$(mktemp -d)
+
+cleanup() {
+    echo "Cleaning up..."
+    rm -rf $WORKDIR
+}
+
+trap cleanup EXIT
+
+cd $WORKDIR
+
+echo "Downloading latest TLD list..."
+wget -O - https://data.iana.org/TLD/tlds-alpha-by-domain.txt | \
+    sed -e 's@^\([^#].*\)@server=/\1/8.8.8.8@' \
+        >/etc/dnsmasq.d/09-forward.tld.conf
+
+echo "Downloading latest China list..."
+wget -O dnsmasq-china-list.zip https://codeload.github.com/felixonmars/dnsmasq-china-list/zip/refs/heads/master
+unzip dnsmasq-china-list.zip
+
+cd dnsmasq-china-list-master
+
+for _conf in accelerated-domains.china google.china apple.china; do
+    awk -F '/' '{ printf("server=/%s/#\n", $2) }' "$_conf.conf" >"/etc/dnsmasq.d/10-forward.$_conf.conf"
+done
+cp bogus-nxdomain.china.conf /etc/dnsmasq.d/10-bogus-nxdomain.china.conf
+
+echo "Restarting dnsmasq..."
+systemctl restart dnsmasq
