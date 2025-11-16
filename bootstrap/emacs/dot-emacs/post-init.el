8e6a58252e06c7d518fb837598fe026c3a384f9c
Wenxin Wang <i@wenxinwang.me>
2025-11-16 22:37:19 +0800

    emacs: basic ui & help

--- bootstrap/emacs/dot-emacs/post-init.el
+++ bootstrap/emacs/dot-emacs/post-init.el
@@ -8,6 +8,43 @@
 (when (bound-and-true-p use-package-compute-statistics)
   (require 'use-package))
 
+;; Basic UI.
+
+;; Blackout: hide minor modes from modeline
+(use-package blackout
+  :demand t)
+
+;; Better help.
+(use-package which-key
+  :straight nil  ; built-in
+  :blackout t
+  :hook ((emacs-startup . which-key-mode))
+
+  :config
+  ;; We configure it so that `which-key' is triggered by typing C-h
+  ;; during a key sequence (the usual way to show bindings). See
+  ;; <https://github.com/justbur/emacs-which-key#manual-activation>.
+  (setq which-key-show-early-on-C-h t)
+  (setq which-key-idle-delay most-positive-fixnum)
+  (setq which-key-idle-secondary-delay 0.05))
+
+;; Clipboard.
+(setq save-interprogram-paste-before-kill t)
+
+;; Scrolling.
+(unless (and (eq window-system 'mac)
+             (bound-and-true-p mac-carbon-version-string))
+  ;; Enables `pixel-scroll-precision-mode' on all operating systems and Emacs
+  ;; versions, except for emacs-mac.
+  ;;
+  ;; Enabling `pixel-scroll-precision-mode' is unnecessary with emacs-mac, as
+  ;; this version of Emacs natively supports smooth scrolling.
+  ;; https://bitbucket.org/mituharu/emacs-mac/commits/65c6c96f27afa446df6f9d8eff63f9cc012cc738
+  (setq pixel-scroll-precision-use-momentum nil) ; Precise/smoother scrolling
+  (pixel-scroll-precision-mode 1))
+
+;; More speed optimizations.
+
 ;; Copied from
 ;; https://github.com/jamescherti/minimal-emacs.d?tab=readme-ov-file#optimization-native-compilation.
 ;; Native compilation enhances Emacs performance by converting Elisp code into
@@ -44,4 +81,25 @@
   ;; A global mode that compiles .el files prior to loading them via `load' or
   ;; `require'. Additionally, it compiles all packages that were loaded before
   ;; the mode `compile-angel-on-load-mode' was activated.
-  (compile-angel-on-load-mode 1))
+  ;; Disable this in debug mode because it may trigger loading of code not defined
+  ;; during normal compilation.
+  (when (not minimal-emacs-debug)
+    (compile-angel-on-load-mode 1)))
+
+;; Theme.
+(load-theme 'modus-operandi t)
+(setq modus-themes-to-toggle '(modus-operandi modus-vivendi)
+      modus-themes-to-rotate modus-themes-items
+      modus-themes-mixed-fonts t
+      modus-themes-variable-pitch-ui t
+      modus-themes-italic-constructs t
+      modus-themes-bold-constructs t
+      modus-themes-completions '((t . (bold)))
+      modus-themes-prompts '(bold)
+      modus-themes-headings
+      '((agenda-structure . (variable-pitch light 2.2))
+        (agenda-date . (variable-pitch regular 1.3))
+        (t . (regular 1.15))))
+(setq modus-themes-common-palette-overrides nil)
+
+(load custom-file 'noerror 'no-message)
