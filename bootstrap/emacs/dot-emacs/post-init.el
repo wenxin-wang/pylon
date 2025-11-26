c94b8a7e3de057c2142bfd473aea719f32db5c8b
Wenxin Wang <i@wenxinwang.me>
2025-11-26 18:32:55 +0800

    emacs: meow-mode with ergonomic keybinds

--- bootstrap/emacs/dot-emacs/post-init.el
+++ bootstrap/emacs/dot-emacs/post-init.el
@@ -62,6 +62,208 @@
   (setq which-key-idle-delay most-positive-fixnum)
   (setq which-key-idle-secondary-delay 0.05))
 
+;; Better repeat.
+(use-package repeat-fu
+  :blackout t
+  ;; repeat-fu defines ~incf~ & ~decf~ instead of using ~cl-lib~,
+  ;; inside a ~eval-when-compile~. This causes trouble for byte-compile
+  ;; and native-compile, where we must load the uncompiled ~repeat-fu.el~
+  ;; when compiling ~repeat-fu-preset-meow.el~. I can control this order
+  ;; for byte-compile by explicitly setting the loading order for ~straight.el~,
+  ;; but I cannot do the same thing for native-compile (as native-compile must
+  ;; use ~.elc~ files instead, so I just disable native-compile for
+  ;; ~repeat-fu-preset-meow.el~.
+  ;; Something I don't understand: I think the macros should be gone in the ~.elc~
+  ;; and native-compile should have nothing to do with them. So how did things
+  ;; happen?
+  ;; :straight (:build (:not compile))
+  :straight (:files ("repeat-fu-preset-meow.el" "repeat-fu.el"))
+  :commands (repeat-fu-mode repeat-fu-execute)
+
+  :custom
+  (repeat-fu-preset 'meow)
+
+  :hook
+  (meow-mode . (lambda ()
+                 (when (and (not (minibufferp)) (not (derived-mode-p 'special-mode)))
+                   (repeat-fu-mode)))))
+
+;; Modal editing.
+(use-package meow
+  ;; Somehow ~meow-global-mode~ does not trigger evaluation
+  ;; of :config block.
+  :demand t
+  :config
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (add-to-list
+   'meow-char-thing-table
+   '(?\( . round))
+  (add-to-list
+   'meow-char-thing-table
+   '(?\[ . square))
+  (add-to-list
+   'meow-char-thing-table
+   '(?\{ . curly))
+
+  ;; Fix digit-arguments.
+  (eval-when-compile
+    (defmacro meow-expand-n (name n)
+      `(defun ,name ()
+         (interactive)
+         (let ((this-command 'meow-expand))
+           (meow-expand ,n)))))
+  (meow-expand-n meow-expand-1 1)
+  (meow-expand-n meow-expand-2 2)
+  (meow-expand-n meow-expand-3 3)
+  (meow-expand-n meow-expand-4 4)
+  (meow-expand-n meow-expand-5 5)
+  (meow-expand-n meow-expand-6 6)
+  (meow-expand-n meow-expand-7 7)
+  (meow-expand-n meow-expand-8 8)
+  (meow-expand-n meow-expand-9 9)
+  (meow-expand-n meow-expand-0 0)
+
+  ;; Copied from https://github.com/meow-edit/meow/issues/506
+  ;; -------------------- ;;
+  ;;         UTILS        ;;
+  ;; -------------------- ;;
+  (defun meow-kill-line ()
+    "Kill till the end of line."
+    (interactive)
+    (let ((select-enable-clipboard meow-use-clipboard))
+      (kill-line)))
+
+  (defun meow-change-line ()
+    "Kill till end of line and switch to INSERT state."
+    (interactive)
+    (meow--cancel-selection)
+    (meow-end-of-thing
+     (car (rassoc 'line meow-char-thing-table)))
+    (meow-change))
+
+  (defun meow-save-clipboard ()
+    "Copy in clipboard."
+    (interactive)
+    (let ((meow-use-clipboard t))
+      (meow-save)))
+
+  (defun meow-smart-reverse ()
+    "Reverse selection or begin negative argument."
+    (interactive)
+    (if (use-region-p)
+        (meow-reverse)
+      (negative-argument nil)))
+
+  (add-to-list
+   'meow-selection-command-fallback
+   '(meow-kill . meow-delete))
+  (meow-motion-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("SPC" . nil)
+   '("S-SPC" . meow-keypad)
+   '("<escape>" . meow-cancel-selection))
+  (meow-define-keys 'normal
+    ;; expansion
+    '("0" . meow-expand-0)
+    '("1" . meow-expand-1)
+    '("2" . meow-expand-2)
+    '("3" . meow-expand-3)
+    '("4" . meow-expand-4)
+    '("5" . meow-expand-5)
+    '("6" . meow-expand-6)
+    '("7" . meow-expand-7)
+    '("8" . meow-expand-8)
+    '("9" . meow-expand-9)
+    '("-" . meow-smart-reverse)
+
+    ;; movement
+    '("k" . meow-prev)
+    '("j" . meow-next)
+    '("h" . meow-left)
+    '("l" . meow-right)
+
+    '("n" . meow-search)
+    '("/" . meow-visit)
+
+    '("<backspace>" . meow-page-up)
+    '("RET" . meow-page-down)
+
+    ;; expansion
+    '("K" . meow-prev-expand)
+    '("J" . meow-next-expand)
+    '("H" . meow-left-expand)
+    '("L" . meow-right-expand)
+
+    '("i" . meow-back-word)
+    '("I" . meow-back-symbol)
+    '("o" . meow-next-word)
+    '("O" . meow-next-symbol)
+
+    '("a" . meow-mark-word)
+    '("A" . meow-mark-symbol)
+    '("w" . meow-line)
+    '("W" . meow-block)
+    '("gl" . meow-goto-line)
+    '("gs" . magit)
+    '("q" . meow-join)
+    '("t" . meow-grab)
+    '("Tp" . meow-pop-grab)
+    '("Tw" . meow-swap-grab)
+    '("Ts" . meow-sync-grab)
+    '("p" . meow-cancel-selection)
+    '("P" . meow-pop-selection)
+    '("." . repeat-fu-execute)
+
+    '("f" . meow-till)
+    '("F" . meow-find)
+
+    '("m" . meow-beginning-of-thing)
+    '("," . meow-end-of-thing)
+    '("M" . meow-inner-of-thing)
+    '("<" . meow-bounds-of-thing)
+
+    ;; editing
+    '("d" . meow-kill)
+    '("D" . meow-kill-line)
+    '("s" . meow-change)
+    '("S" . meow-change-line)
+    '("c" . meow-save)
+    '("C" . meow-save-clipboard)
+    '("v" . meow-yank)
+    '("V" . meow-yank-pop)
+
+    '("e" . meow-insert)
+    '("E" . meow-open-above)
+    '("r" . meow-append)
+    '("R" . meow-open-below)
+
+    '("u" . undo-only)
+    '("U" . undo-redo)
+
+    '("b" . open-line)
+    '("B" . split-line)
+
+    '("[" . indent-rigidly-left-to-tab-stop)
+    '("]" . indent-rigidly-right-to-tab-stop)
+
+    ;; prefix y
+    '("yf" . meow-comment)
+    '("yt" . meow-start-kmacro-or-insert-counter)
+    '("yr" . meow-start-kmacro)
+    '("ye" . meow-end-or-call-kmacro)
+    ;; ...etc
+
+    ;; prefix ;
+    '(";f" . save-buffer)
+    '(";F" . save-some-buffers)
+    '(";d" . meow-query-replace-regexp)
+    ;; ... etc
+
+    ;; ignore escape
+    '("<escape>" . meow-cancel-selection))
+  (meow-global-mode))
+
 ;; Clipboard.
 (setq save-interprogram-paste-before-kill t)
 
@@ -88,6 +290,7 @@
 ;; Ensure adding the following compile-angel code at the very beginning
 ;; of your `~/.emacs.d/post-init.el` file, before all other packages.
 (use-package compile-angel
+  :blackout t
   :demand t
   :custom
   ;; Set `compile-angel-verbose` to nil to suppress output from compile-angel.
@@ -95,6 +298,8 @@
   (compile-angel-verbose t)
 
   :config
+  ;; See above ~use-package repeat-fu~ for explanation.
+  (push "/repeat-fu-preset-meow.el" compile-angel-excluded-files)
   ;; The following directive prevents compile-angel from compiling your init
   ;; files. If you choose to remove this push to `compile-angel-excluded-files'
   ;; and compile your pre/post-init files, ensure you understand the
--- bootstrap/emacs/dot-emacs/pre-early-init.el
+++ bootstrap/emacs/dot-emacs/pre-early-init.el
@@ -6,3 +6,9 @@
 (setq minimal-emacs-ui-features '(tooltips))
 (setq minimal-emacs-package-initialize-and-refresh nil)
 (setq minimal-emacs-load-compiled-init-files t)
+;; We use ~compile-angle~ instead.
+(setq
+ native-comp-deferred-compilation nil
+ native-comp-jit-compilation nil
+ package-native-compile nil
+ minimal-emacs-setup-native-compilation nil)
