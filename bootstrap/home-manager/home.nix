4427f23a908c02bc96c3b71d1ec23bc31edc7728
Wenxin Wang <i@wenxinwang.me>
2025-12-03 18:25:41 +0800

    emacs: python lsp

--- bootstrap/home-manager/home.nix
+++ bootstrap/home-manager/home.nix
@@ -33,6 +33,7 @@ in
     pkgs.emacs
     pkgs.fd
     pkgs.jsonnet-language-server
+    pkgs.pyright
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
