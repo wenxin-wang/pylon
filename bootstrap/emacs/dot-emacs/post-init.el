8795e7b29a877520a28c8779e14cca129854a9d2
Wenxin Wang <wenxin@pony.ai>
2025-12-03 11:45:07 +0800

    emacs: easysession

--- bootstrap/emacs/dot-emacs/post-init.el
+++ bootstrap/emacs/dot-emacs/post-init.el
@@ -694,6 +694,37 @@
 ;;   ;; Somehow the autoload for bufler-workspace-workspaces-as-tabs-mode is incorrect.
 ;;   (require 'bufler-workspace-tabs))
 
+;; The easysession Emacs package is a session manager for Emacs that can persist
+;; and restore file editing buffers, indirect buffers/clones, Dired buffers,
+;; windows/splits, the built-in tab-bar (including tabs, their buffers, and
+;; windows), and Emacs frames. It offers a convenient and effortless way to
+;; manage Emacs editing sessions and utilizes built-in Emacs functions to
+;; persist and restore frames.
+(use-package easysession
+  :commands (easysession-switch-to
+             easysession-save-as
+             easysession-save-mode
+             easysession-load-including-geometry)
+
+  :custom
+  (easysession-mode-line-misc-info t)  ; Display the session in the modeline
+  (easysession-save-interval (* 10 60))  ; Save every 10 minutes
+
+  :bind
+  ;; Key mappings:
+  ;; C-c l for switching sessions
+  ;; and C-c s for saving the current session
+  ("C-c l" . 'easysession-switch-to)
+  ("C-c s" . 'easysession-save-as)
+
+  :init
+  ;; The depth 102 and 103 have been added to to `add-hook' to ensure that the
+  ;; session is loaded after all other packages. (Using 103/102 is particularly
+  ;; useful for those using minimal-emacs.d, where some optimizations restore
+  ;; `file-name-handler-alist` at depth 101 during `emacs-startup-hook`.)
+  (add-hook 'emacs-startup-hook #'easysession-load-including-geometry 102)
+  (add-hook 'emacs-startup-hook #'easysession-save-mode 103))
+
 ;; More speed optimizations.
 
 ;; Copied from
