7f8e201938284aa1208fb5f3a17d2e158692fbc7
Wenxin Wang <i@wenxinwang.me>
2026-02-04 10:27:21 +0800

    emacs: upgrade packages

--- bootstrap/emacs/dot-emacs/post-init.el
+++ bootstrap/emacs/dot-emacs/post-init.el
@@ -622,8 +622,8 @@ the macro key instead of the original key."
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
+   consult-source-bookmark consult-source-file-register
+   consult-source-recent-file consult-source-project-recent-file
    ;; :preview-key "M-."
    :preview-key '(:debounce 0.4 any))
   (setq consult-narrow-key "<"))
